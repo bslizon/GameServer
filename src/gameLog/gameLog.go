43e67fbcdb4e0416c711c2f7a13697b6fe37170c
BSLizon <bslizon@gmail.com>
2016-08-06 03:42:28 +0800

    fix

--- src/gameLog/gameLog.go
+++ src/gameLog/gameLog.go
@@ -16,13 +16,17 @@ var logger *log.Logger
 var level = globalConfig.LOG_LEVEL
 
 func init() {
-	expFilePtr, err := os.OpenFile("E:/result.txt", os.O_CREATE | os.O_WRONLY, 0600)
-	if err != nil {
-		fmt.Println(err)
-		return
+	if globalConfig.USE_LOG_FILE {
+		expFilePtr, err := os.OpenFile(globalConfig.GATESERVER_LOG_FILE_PATH, os.O_CREATE | os.O_WRONLY, 0600)
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+		logFileWriter = bufio.NewWriter(expFilePtr)
+		logger = log.New(logFileWriter, "", log.Ldate | log.Ltime | log.Lmicroseconds | log.Llongfile)
+	} else {
+		logger = log.New(os.Stdout, "", log.Ldate | log.Ltime | log.Lmicroseconds | log.Llongfile)
 	}
-	logFileWriter = bufio.NewWriter(expFilePtr)
-	logger = log.New(logFileWriter, "", log.Ldate | log.Ltime | log.Lmicroseconds | log.Llongfile)
 }
 
 func Debug(v interface{}) {
@@ -46,21 +50,27 @@ func Warn(v interface{}) {
 func Error(v interface{}) {
 	if logLevel.ERROR >= level {
 		logger.Output(2, fmt.Sprintln("[ERROR]", v))
-		logFileWriter.Flush()
+		if logFileWriter != nil{
+			logFileWriter.Flush()
+		}
 	}
 }
 
 func Panic(v interface{}) {
 	if logLevel.PANIC >= level {
 		logger.Output(5, fmt.Sprintln("[PANIC]", v))
-		logFileWriter.Flush()
+		if logFileWriter != nil{
+			logFileWriter.Flush()
+		}
 	}
 }
 
 func Fatal(v interface{}) {
 	if logLevel.FATAL >= level {
 		logger.Output(2, fmt.Sprintln("[FATAL]", v))
-		logFileWriter.Flush()
+		if logFileWriter != nil{
+			logFileWriter.Flush()
+		}
 		os.Exit(1)
 	}
 }
--- src/globalConfig/globalConfig.go
+++ src/globalConfig/globalConfig.go
@@ -6,4 +6,7 @@ import (
 
 const (
 	LOG_LEVEL = logLevel.DEBUG
+	GATESERVER_LOG_FILE_PATH = "E:/GateServerLog.txt"
+	LOGICSERVER_LOG_FILE_PATH = "E:/LogicServerLog.txt"
+	USE_LOG_FILE = true
 )
