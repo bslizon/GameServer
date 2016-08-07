5c606b186a28d45821b5eb446d10739df93a4ee8
BSLizon <bslizon@gmail.com>
2016-08-07 14:30:19 +0800

    整体写死为uint32，优化发送逻辑

--- src/GateServer/main.go
+++ src/GateServer/main.go
@@ -2,9 +2,20 @@ package main
 
 import (
 	"GateServer/Server"
+	//"os"
+	//"runtime/pprof"
+	//"GateServer/config"
+	//"time"
 )
 
 func main() {
+	//f, _ := os.Create(config.PROFILE_FILE)
+	//pprof.StartCPUProfile(f)  // 开始cpu profile，结果写到文件f中
+	//defer pprof.StopCPUProfile()  // 结束profile
 	Server.GateServer = Server.NewTcpPackServer()
 	Server.GateServer.Start()
+
+	//select {
+	//case <-time.After(30 * time.Second):
+	//}
 }
