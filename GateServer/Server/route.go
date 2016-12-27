5a14bcb8491d3de841d479e83d843dafe363f9a1
BSLizon <bslizon@gmail.com>
2016-12-27 16:49:53 +0800

    remove panic handling

--- GateServer/Server/route.go
+++ GateServer/Server/route.go
@@ -9,46 +9,13 @@ import (
 )
 
 // 必须保证是线程安全的
-func (svr *TcpPackServer) RoutePackIn(p *Pack.Pack) (rerr error) {
-	// panic转error
-	defer func() {
-		if x := recover(); x != nil {
-			switch value := x.(type) {
-			case error:
-				rerr = value
-			case string:
-				rerr = errors.New(value)
-			default:
-				rerr = errors.New(fmt.Sprintf("unknown panic: %#v. ", value))
-			}
-		}
-	}()
-	////////////////////////////////////////////////////////////////////
-
-	svr.RoutePackOut(p)
+func (svr *TcpPackServer) RoutePackIn(p *Pack.Pack) error {
 	gLog.Debug(fmt.Sprintf("routing. sid: %d data: %v", p.Sid, p.Data))
-	////////////////////////////////////////////////////////////////////
-	rerr = nil
-	return
+	return svr.RoutePackOut(p)
 }
 
 // 必须保证是线程安全的
-func (svr *TcpPackServer) RoutePackOut(p *Pack.Pack) (rerr error) {
-	// panic转error
-	defer func() {
-		if x := recover(); x != nil {
-			switch value := x.(type) {
-			case error:
-				rerr = value
-			case string:
-				rerr = errors.New(value)
-			default:
-				rerr = errors.New(fmt.Sprintf("unknown panic: %#v ", value))
-			}
-		}
-	}()
-	////////////////////////////////////////////////////////////////////
-
+func (svr *TcpPackServer) RoutePackOut(p *Pack.Pack) error {
 	if p.Sid == config.BROCASTING_SID {
 		lkMap := svr.GetLinkMapCopy()
 		for _, lk := range lkMap {
@@ -64,16 +31,11 @@ func (svr *TcpPackServer) RoutePackOut(p *Pack.Pack) (rerr error) {
 			er := lk.PutBytes(p.Data)
 			if er != nil {
 				gLog.Error(er)
-				rerr = errors.New(error.Error(er) + " put wtSyncChan failed.")
-				return
+				return errors.New(error.Error(er) + " put wtSyncChan failed.")
 			}
 		} else {
-			rerr = errors.New("invalid sid.")
-			return
+			return errors.New("invalid sid.")
 		}
 	}
-
-	////////////////////////////////////////////////////////////////////
-	rerr = nil
-	return
+	return nil
 }
